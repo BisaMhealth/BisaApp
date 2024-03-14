b2510f3cd54c3d57df015f11e50522ac8e47a65e
Aquarius-blake <danbaid72@gmail.com>
2024-03-14 15:28:26 +0000

    update 8 files

--- lib/ui/home/Water_drinking/Water_home.dart
+++ lib/ui/home/Water_drinking/Water_home.dart
@@ -12,7 +12,7 @@ class _WaterHomeState extends State<WaterHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 137, 211, 245),
+      //backgroundColor: const Color.fromARGB(255, 137, 211, 245),
       appBar: AppBar(
         backgroundColor: Colors.lightBlueAccent,
         elevation: 0.0,
@@ -40,6 +40,56 @@ class _WaterHomeState extends State<WaterHome> {
             color: Colors.white,
           ),
           ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 10
+        ),
+        child: Column(
+          children: [
+            Container(
+              // padding: EdgeInsets.symmetric(
+              //   horizontal: 10
+              // ),
+              decoration: BoxDecoration(),
+              child: Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width*0.9,
+                //padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.9),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20)
+                    )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(),
+                    Container(
+                      height: 140,
+                      width: MediaQuery.of(context).size.width*0.9,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/imgs/waves.png',
+                            ),
+                            fit: BoxFit.cover
+                          )
+                      ),
+                      child: Text(""),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container()
+          ],
+        ),
       ),
     );
   }
