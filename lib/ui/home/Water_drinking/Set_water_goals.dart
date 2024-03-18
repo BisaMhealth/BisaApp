3bb7d9726a34a7c10f663e1b2241ff95f51dcb05
Aquarius-blake <danbaid72@gmail.com>
2024-03-18 15:17:05 +0000

    update 8 files

--- lib/ui/home/Water_drinking/Set_water_goals.dart
+++ lib/ui/home/Water_drinking/Set_water_goals.dart
@@ -1,4 +1,7 @@
+
+import 'package:bisa_app/ui/home/Water_drinking/Water_quantity_card.dart';
 import 'package:flutter/material.dart';
+import 'package:waterfall_flow/waterfall_flow.dart';
 
 class WaterGoals extends StatefulWidget {
   const WaterGoals({super.key});
@@ -8,6 +11,20 @@ class WaterGoals extends StatefulWidget {
 }
 
 class _WaterGoalsState extends State<WaterGoals> {
+
+final _formkey = GlobalKey<FormState>();
+TextEditingController _goalController = TextEditingController();
+final goalfocusNode = FocusNode();
+ int Selectedindex = 6;
+List images = [
+  'assets/imgs/cup.png',
+  'assets/imgs/glass1.png',
+  'assets/imgs/mug.png',
+  'assets/imgs/bottle1.png',
+  'assets/imgs/bottle2.png',
+  'assets/imgs/jar.png'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +86,7 @@ class _WaterGoalsState extends State<WaterGoals> {
                   )
                 ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Center(
                       child: Text(
@@ -80,6 +98,83 @@ class _WaterGoalsState extends State<WaterGoals> {
                           )
                         ),
                     ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Form(
+                        key: _formkey,
+                        // child: CustomTextField(
+                        //   isEnabled: true,
+                        //   fieldController: _goalController, 
+                        //   fieldValidator: Validator.textValidator, 
+                        //   currentFocus: goalfocusNode, 
+                        //   fieldHintText: "Set your daily goal", 
+                        //   fieldTextInputAction: TextInputAction.done
+                        //   ),
+                        child: TextFormField(
+                          controller: _goalController,
+                          decoration: InputDecoration(
+                            
+                          ),
+                        )
+                        ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      "Choose your water container size:",
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                     SizedBox(
+                      height: 20,
+                    ),
+                    WaterfallFlow.builder(
+      shrinkWrap: true,
+    //  physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 10,
+      ),
+      gridDelegate: const SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 15.0,
+        mainAxisSpacing: 55.0,
+      ),
+      itemCount: 6,
+      itemBuilder: (BuildContext context, int index) {
+        return InkWell(
+          onTap: (){
+            setState(() {
+              Selectedindex = index;
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: Selectedindex == index? [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ]:null,
+            ),
+            child: WaterQuantity(
+              image: images[index],
+              quantity: (index + 1) * 100,
+            ),
+          ),
+        );
+      },
+    )
                   ],
                 ),
               ),
