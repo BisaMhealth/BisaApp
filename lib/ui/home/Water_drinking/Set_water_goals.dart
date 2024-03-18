17e34c69cc6d7644be134f1182baafcc32fe188d
Aquarius-blake <danbaid72@gmail.com>
2024-03-18 13:47:45 +0000

    update Water_dash.dart

---
+++ lib/ui/home/Water_drinking/Set_water_goals.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class WaterGoals extends StatefulWidget {
+  const WaterGoals({super.key});
+
+  @override
+  State<WaterGoals> createState() => _WaterGoalsState();
+}
+
+class _WaterGoalsState extends State<WaterGoals> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 10
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  // borderRadius: BorderRadius.only(
+                  //   bottomLeft: Radius.circular(30),
+                  //   bottomRight: Radius.circular(30)
+                  // )
+                ),
+                child:  Row(
+                  children: [
+                    GestureDetector(
+            onTap: (){
+              Navigator.pop(context);
+            },
+            child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Icon(
+                            Icons.arrow_back_ios_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+          ),
+                  ],
+                ),
+               
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height - 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)
+                  )
+                ),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        "Set Water Goals",
+                        style: TextStyle(
+                          color: Colors.lightBlueAccent,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+                          )
+                        ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/home/Water_drinking/Water_dash.dart
+++ lib/ui/home/Water_drinking/Water_dash.dart
@@ -1,6 +1,9 @@
 
 
+import 'package:bisa_app/animation/PageTransition.dart';
+import 'package:bisa_app/ui/home/Water_drinking/Set_water_goals.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class WaterDash extends StatefulWidget {
   const WaterDash({super.key});
@@ -72,14 +75,66 @@ class _WaterDashState extends State<WaterDash> {
                     vertical: 20
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("sdsd")
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextButton(
+                        onPressed: (){
+                          PageAnimateNoRep(context, PageTransitionType.fade, WaterGoals());
+                        },
+                        child: const Text(
+                          "Set your daily goal",
+                          style: TextStyle(
+                            color: Colors.lightBlueAccent,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                            ),
+                          ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                        ),
+                      Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Current Daily Goal",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 18
+                              ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                                ),
+                                Text(
+                                  "1500ml/day",
+                                  style: TextStyle(
+                                    fontSize: 38
+                                  ),
+                                  )
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
               )
             ],
           ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/imgs/waterdrop.png'),
+            fit: BoxFit.cover
+            )
         ),
       ),
       );
